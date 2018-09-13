199c13b1c78119b54920002b1f7b236b9badc37d
brakenj <e.son1628@gmail.com>
2018-09-13 17:42:55 +0800

    initial commit hello-world api

---
+++ hello-world/src/main/java/com/fimc/hello/model/Person.java
@@ -0,0 +1,14 @@
+package com.fimc.hello.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private String birthDate;
+}
